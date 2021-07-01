91d6d40c2f93a49b9de2d9a56a40f93afca68e4f
Nam Le <lethanhnam.l3i@gmail.com>
2021-07-01 17:02:04 +0200

    Include body-parser and include in the server code so that login pwd in the request body can be parsed

--- example/index.js
+++ example/index.js
@@ -3,11 +3,17 @@ async function init(){
 const express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
 
 const app = express();
 const port = 3000;
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// require body-parser to properly login:pwd in request body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({limit: '1mb'}));
 
 // Static content.
 global.STATIC_DIR = path.join(__dirname, "static");
@@ -64,12 +70,40 @@ app.use('/hauth/mod', hauth.modUser);
 
 
 /* function hauth.control */
-app.use('/', hauth.control);
+// app.use('/', hauth.control);
 
-app.get('/', (req, res) => {
+app.use('/', function (req, res, next) {
+  hauth.control(req, res, next)
   // res.send('Welcome to the hauth authentication page!')
-  res.sendFile(hauthParams.errorPage.login)
+  // res.sendFile(hauthParams.errorPage.login)
 })
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handlers
+
+// development error handler: will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler: no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example server running at http://localhost:${port}`)
